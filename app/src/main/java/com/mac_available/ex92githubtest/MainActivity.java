2841ae709499c5cde8efc0519e7cbd54a3fff2ed
Youngwoo1128 <77264918+Youngwoo1128@users.noreply.github.com>
2021-02-05 14:20:14 +0900

    Update MainActivity.java

--- app/src/main/java/com/mac_available/ex92githubtest/MainActivity.java
+++ app/src/main/java/com/mac_available/ex92githubtest/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Git Hub");
+        
+       int a;
+       
     }
 }
